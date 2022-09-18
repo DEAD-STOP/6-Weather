b8a6e9cdf33cb56cd7bd8cdc8bbc7efc4ae0643c
DEAD-STOP <arstop98@gmail.com>
2022-09-18 18:20:04 -0500

    api

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const theKey = `f843a7920383c594518d52947c4bbf83`;
-const apiUrl = `https://api.openweathermap.org`;
+const apiRootUrl = 'https://api.openweathermap.org';
+
+async function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    const apiUrl = `${apiRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${theKey}`;
+  
+    await fetch(apiUrl)
+      .then(function (res) {
+        console.log(res);
+        return res.json();
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+  fetchWeather('Austin');
